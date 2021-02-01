ffff17879809ebfc9380dd84e277dcee294e322b
YuriShido <sydonce57@gmail.com>
2021-02-01 13:12:39 -0800

    first commit

--- 00-movie-app/app.js
+++ 00-movie-app/app.js
@@ -3,31 +3,39 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 // const main = get DOM element of main
+const main = document.querySelector('#main');
 // const form = get DOM element of form
+const form = document.querySelector('#form');
 // const search = get DOM element of search
-
+const search = document.querySelector('#search');
 // Get initial movies
 getMovies(API_URL)
 
 //Create an async function of getMovies 
 //------assign a response to a fetch/axios method
-//------call showMovies function and pass the response as argument
+async function getMovies(url) {
+    const response = await axios.get(url);
+    console.log(response);
+    //------call showMovies function and pass the response as argument
+    showMovies(response.data.results);
+}
 
 function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
         //destructure the movie object
-        // const { ???, ??? ,vote_average } = movie //include the "vote_average"
+        const {title, poster_path, vote_average, overview } = movie //include the "vote_average"
 
         //create a div element
+        const movieEl = document.createElement('div');
         //add a "movie" class in that div 
-
+        movieEl.classList.add('movie');
         //manipulate the newly created element's innerHTML (I called it movieEl in this sample)
         movieEl.innerHTML = `
-            <img src="" alt="">
+            <img src="${IMG_PATH}${poster_path}" alt="">
             <div class="movie-info">
-          <h3></h3>
+          <h3>${title}</h3>
           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
@@ -36,6 +44,8 @@ function showMovies(movies) {
         </div>
         `
         //append the movieEl to main
+        main.appendChild(movieEl);
+
     })
 }
 
@@ -51,14 +61,15 @@ function getClassByRate(vote) {
 
 form.addEventListener('submit', (e) => {
     //prevent refresh
-
+    e.preventDefault;
     //const searchTerm = ???  //assign the value of the input's value
-
+    const searchTerm = search.value;
+    //call the getMovies function and pass the concatenated value of SEARCH_API + searchTerm
     if(searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
-        //call the getMovies function and pass the concatenated value of SEARCH_API + searchTerm
 
         //reset the input's value
+        search.value ="";
     } else {
         window.location.reload()
     }
